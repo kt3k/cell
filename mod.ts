02eb393b8e0368199d1bd785155ad852195ac7c2
Yoshiya Hinosawa <stibium121@gmail.com>
2024-09-15 23:29:06 +0900

    chore: bump to v0.3.8

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-/*! Cell v0.3.7 | Copyright 2024 Yoshiya Hinosawa and Capsule contributors | MIT license */
+/*! Cell v0.3.8 | Copyright 2024 Yoshiya Hinosawa and Capsule contributors | MIT license */
 import { documentReady, logEvent } from "./util.ts"
 export { signal } from "@kt3k/signal"
 
